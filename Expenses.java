4e23612535256d217da23c834d719ac5a5a1f18a
spinfern-o <larrywzh59@gmail.com>
2026-09-14 13:16:46 -0400

    chagned variables to accompany local business statistics, removed direct and operatting expenses to cfodashboard.java

--- Expenses.java
+++ Expenses.java
@@ -2,58 +2,111 @@ public class Expenses {
 
     int eMonthNum;
     double COGS;
+    double packaging;
     double rent;
+    double utilities;
+    double maintenance;
+    double hardware;
+    double insurance;
+    double marketing;
+    double creditCardFees;
+    double businessLicense;
+    double telephone;
+    double employeeMeals;
     double tax;
     double labor;
-    double directExpenses;
-    double operatingExpenses;
 
     public Expenses(
         int eMonthNum,
         double COGS,
+        double packaging,
         double rent,
+        double utilities,
+        double maintenance,
+        double hardware,
+        double insurance,
+        double marketing,
+        double creditCardFees,
+        double businessLicense,
+        double telephone,
+        double employeeMeals,
         double tax,
-        double labor,
-        double directExpenses,
-        double operatingExpenses
+        double labor
     ) {
         this.eMonthNum = eMonthNum;
         this.COGS = COGS;
+        this.packaging = packaging;
         this.rent = rent;
+        this.utilities = utilities;
+        this.maintenance = maintenance;
+        this.hardware = hardware;
+        this.insurance = insurance;
+        this.marketing = marketing;
+        this.creditCardFees = creditCardFees;
+        this.businessLicense = businessLicense;
+        this.telephone = telephone;
+        this.employeeMeals = employeeMeals;
         this.tax = tax;
         this.labor = labor;
-        this.directExpenses = directExpenses;
-        this.operatingExpenses = operatingExpenses;
     }
 
-    public int getExpensesMonthNum(){
+    public int eMonthNum() {
         return eMonthNum;
     }
-    public double getDirectExpenses(){
-        return directExpenses;
-    }
 
-    public double getOperatingExpenses(){
-        return operatingExpenses;
-    }
-
-    public double getTotalExpenses(){
-        return directExpenses + operatingExpenses;
-    }
-
-    public double getRent(){
-        return rent;
-    }
-
-    public double getCOGS(){
+    public double COGS() {
         return COGS;
     }
 
-    public double getLabor(){
-        return labor;
+    public double packaging() {
+        return packaging;
     }
 
-    public double getTax(){
+    public double rent() {
+        return rent;
+    }
+
+    public double utilities() {
+        return utilities;
+    }
+
+    public double maintenance() {
+        return maintenance;
+    }
+
+    public double hardware() {
+        return hardware;
+    }
+
+    public double insurance() {
+        return insurance;
+    }
+
+    public double marketing() {
+        return marketing;
+    }
+
+    public double creditCardFees() {
+        return creditCardFees;
+    }
+
+    public double businessLicense() {
+        return businessLicense;
+    }
+
+    public double telephone() {
+        return telephone;
+    }
+
+    public double employeeMeals() {
+        return employeeMeals;
+    }
+
+    public double tax() {
         return tax;
+    }
+
+    public double labor() {
+        return labor;
     }
 }
